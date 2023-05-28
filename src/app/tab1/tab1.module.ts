5b58dd3296c81ff7fc0e2f91e081f33c61402dbb
Chico Cristiano <felixbat87@gmail.com>
2023-05-28 18:23:03 -0500

    Actualizacion de slide a SWIPEER

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-
+import { PipesModule } from '../pipes/pipes.module';
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    PipesModule,
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
 import {Pelicula} from '../interfaces/interfaces'
+import { register } from 'swiper/element/bundle';
+register();
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+
 })
 export class Tab1Page implements OnInit{
+
+  URL:String= "https://image.tmdb.org/t/p/w500";
   peliculasRecientes:Pelicula[]=[];
   constructor(private moviesService:MoviesService) {}
 
 
   ngOnInit(){
+
 
     this.moviesService.getFeature().subscribe
     (resp=>{
